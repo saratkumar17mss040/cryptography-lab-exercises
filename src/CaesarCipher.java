4d1b252f1381d0333db6f297a157a692dd408852
sarath <sarathdon9245@gmail.com>
2021-07-09 10:58:52 +0530

    🚀 Add - caeser cipher

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -9,77 +9,29 @@ public class CaesarCipher {
         return ((!str.equals("")) && (str != null) && (str.matches("^[a-zA-Z]*$")));
     }
 
-    // public static boolean validateString(String str) {
-    // str = str.toLowerCase();
-    // char[] charArray = str.toCharArray();
-    // for (int i = 0; i < charArray.length; i++) {
-    // char ch = charArray[i];
-    // if (!(ch >= 'a' && ch <= 'z')) {
-    // return false;
-    // }
-    // }
-    // return true;
-    // }
-
-    public static StringBuffer encryptText(String text, int shifts) {
-
-        StringBuffer result = new StringBuffer();
-
-        for (int i = 0; i < text.length(); i++) {
-            if (Character.isUpperCase(text.charAt(i))) {
-                // getting the ascii code + noshiftd - A's ascii code % tnofa + A's ascii code
-                char ch = (char) (((int) text.charAt(i) + shifts - 65) % 26 + 65);
-                result.append(ch);
-            } else {
-                // getting the ascii code + noshiftd - a's ascii code % tnofa + a's ascii code
-                char ch = (char) (((int) text.charAt(i) + shifts - 97) % 26 + 97);
-                result.append(ch);
-            }
-        }
-        return result;
-    }
-
-    public static StringBuffer caeserEncryptDecrypt(String encryptOrDecrypt, String text, int shifts) {
+    public static String caeserEncryptDecrypt(String encryptOrDecrypt, String text, int shifts) {
 
         if (encryptOrDecrypt.equals("decrypt")) {
-            shifts = -shifts;
+            // shift = tnoal - shifts
+            shifts = 26 - shifts;
         }
 
-        StringBuffer result = new StringBuffer();
+        String result = "";
+        char ch;
 
         for (int i = 0; i < text.length(); i++) {
             if (Character.isUpperCase(text.charAt(i))) {
                 // getting the ascii code + noshiftd - A's ascii code % tnofa + A's ascii code
-                char ch = (char) (((int) text.charAt(i) + shifts - 65) % 26 + 65);
-                result.append(ch);
+                ch = (char) (((int) text.charAt(i) + shifts - 65) % 26 + 65);
+                result += ch;
             } else {
                 // getting the ascii code + noshiftd - a's ascii code % tnofa + a's ascii code
-                char ch = (char) (((int) text.charAt(i) + shifts - 97) % 26 + 97);
-                result.append(ch);
+                ch = (char) (((int) text.charAt(i) + shifts - 97) % 26 + 97);
+                result += ch;
             }
         }
         return result;
     }
-
-    // public static StringBuffer decryptText(String text, int shifts) {
-
-    // StringBuffer result = new StringBuffer();
-
-    // for (int i = 0; i < text.length(); i++) {
-    // if (Character.isUpperCase(text.charAt(i))) {
-    // // getting the ascii code + noshiftd - A's ascii code % tnofa + A's ascii
-    // code
-    // char ch = (char) (((int) text.charAt(i) - shifts - 65) % 26 + 65);
-    // result.append(ch);
-    // } else {
-    // // getting the ascii code + noshiftd - a's ascii code % tnofa + a's ascii
-    // code
-    // char ch = (char) (((int) text.charAt(i) - shifts - 97) % 26 + 97);
-    // result.append(ch);
-    // }
-    // }
-    // return result;
-    // }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -93,9 +45,8 @@ public class CaesarCipher {
             String decryptedText = caeserEncryptDecrypt("decrypt", encryptedText, shifts).toString();
             System.out.println("Encrypted text: " + encryptedText);
             System.out.println("Decrypted text: " + decryptedText);
-        } else {
+        } else
             System.out.println("Please enter all the text in alphabets !");
-        }
     }
 
 }
